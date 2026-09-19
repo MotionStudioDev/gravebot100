58647562350ae6985d198c5dc227557156fd4a03
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2026-09-19 23:01:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: process.env.PREFIX || '!',
+  prefix: process.env.PREFIX || 'g!',
   token: process.env.DISCORD_TOKEN,
   owners: [process.env.OWNER_ID || '702901632136118273'],
 };
